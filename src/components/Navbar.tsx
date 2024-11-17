7774eef5332d914c444589297a529cafe30cf96e
Tanmoy Ganguly <xanmoy.tsx@gmail.com>
2024-11-17 19:55:05 +0530

    created navbar & footer

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import Link from 'next/link'
+import Menu from './Menu'
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
+        <div className='h-20 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 relative'>
+            <div className="h-full flex items-center justify-between">
+                {/* Mobile  */}
+                <Link href="/">
+                    <div className="text-2xl tracking-wide">
+                        <h1>
+                            ESTORE
+                        </h1>
+                   </div>
+                </Link>
+                <Menu />
+           </div>
+            
         </div>
     )
 }
